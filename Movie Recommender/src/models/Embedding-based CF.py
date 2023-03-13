8b0056088f770cc471c106b3b1e6e7fd482733f3
Vincent Lewi <vincentlewii@gmail.com>
2023-03-13 17:05:11 +0800

    Vercel deployment testing

--- Movie Recommender/src/models/Embedding-based CF.py
+++ Movie Recommender/src/models/Embedding-based CF.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 import pandas as pd
 import numpy as np
+# import tensorflow as tf
+# from tensorflow.keras.models import load_model
 
-df = pd.read_csv('datasets/movielens_original/ratings.csv')
-df_movie = pd.read_csv('datasets/movielens_original/movies.csv')
+# df = pd.read_csv('datasets/movielens_original/ratings.csv')
+# df_movie = pd.read_csv('datasets/movielens_original/movies.csv')
 
-import tensorflow as tf
-from tensorflow.keras.models import load_model
+app = Flask(__name__)   
 
-new_model = load_model('saved_model/my_model')
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/recommend', methods=['GET', 'POST'])
+def index():
+    # GET request
+    if request.method == 'GET':
+        return 'GET'
+
+    # POST request
+    if request.method == 'POST':
+        return 'POST'
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
+
+# new_model = load_model('saved_model/my_model')
